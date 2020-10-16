0f6c2091634459f3b2496ce53b2da922d20fdb94
srwayne00 <srwayne00@gmail.com>
2020-10-15 21:40:47 -0300

    "second commit day2"

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,52 @@
+const options = {
+    dragging: false,
+    touchZoom: false,
+    doubleClickZoom: false,
+    scrollWheelZoom: false,
+    zoomControl: false
+}
+
+//create map
+const map = L.map('mapid', options).setView([-27.222633,-49.6455874], 15);
+/*-23.534212, -47.416210   /  51.505, -0.09 */
+
+//create and add tileLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+//create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29,68],
+    popupAnchor: [170,2]
+})
+
+//create and add marker
+L
+.marker([-27.222633,-49.6455874], { icon })
+.addTo(map)
+
+/*image galery*/
+
+function selectImage(event) {
+    const button = event.currentTarget
+
+    //remover todas as classes .active
+    const buttons = document.querySelectorAll(".images button")
+    buttons.forEach(removeActiveClass)
+
+    function removeActiveClass(button) {
+        button.classList.remove("active")
+    }
+
+    //selecionar a imagem clicada
+    const image = button.children[0]
+    const imageContainer = document.querySelector(".orphanage-details > img")
+    
+    //atualizar p container de imagem
+    imageContainer.src = image.src
+
+    // adicionar a classe .active para este botão
+    button.classList.add("active")
+
+}
---
+++ public/scripts/page-orphanages.js
@@ -0,0 +1,29 @@
+//create map
+const map = L.map('mapid').setView([-27.222633,-49.6455874], 15);
+/*-23.534212, -47.416210   /  51.505, -0.09 */
+
+//create and add tileLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+//create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29,68],
+    popupAnchor: [170,2]
+})
+
+//createpopup overlay
+const popup = L.popup({
+    closeButton: false,
+    className:'map-popup',
+    minWidth:240,
+    minHeight:240
+}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"></a>')
+
+
+//create and add marker
+L
+.marker([-27.222633,-49.6455874], { icon })
+.addTo(map)
+.bindPopup(popup)
